17dfb4b4c3914092d21c14cffe5aa48f08ca5654
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-03-13 14:50:13 -0300

    hotfix: remove schelules from type class

--- src/types/Class.ts
+++ src/types/Class.ts
@@ -3,17 +3,10 @@ type Duration = {
 	end: string;
 };
 
-type Schedule = {
-	day: string;
-	start: string;
-	end: string;
-};
-
 export type Class = {
 	id: number;
 	title: string;
 	professor: string;
 	place: string;
 	duration: Duration;
-	schedules: Schedule[];
 };
